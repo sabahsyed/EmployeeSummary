dec1953f9cada5d35468c972e56ed62e7353d6f8
Sabah Syed <sabisyed@gmail.com>
2020-05-07 03:08:38 -0700

    Added all prompts to app.js

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,14 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee{
+    constructor(name,email,id,github){
+        super(name,email,id);
+        this.github = github;
+    }
+    getGithub(){
+        return this.github;
+    }
+    getRole(){
+        return this.Engineer;
+    }
+}
--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,15 @@
-// TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Intern extends Employee{
+    constructor(name,email,id,school){
+        super(name,email,id);
+        this.school = school;
+    }
+    getSchool(){
+        return this.school;
+    }
+    getRole(){
+        return this.Intern;
+    }
+
+}
